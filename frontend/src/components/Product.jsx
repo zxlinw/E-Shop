faa4b483648251db5bcfddede7ee466a0c581e78
Zilin <hannahzweng@gmail.com>
2024-08-09 18:53:54 -0400

    ratings

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import {React} from 'react'
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -10,10 +11,14 @@ const Product = ({ product }) => {
         </a>
         <Card.Body>
             <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
+                <Card.Title as="div" className='product-title'>
                     <strong>{product.name}</strong>
                 </Card.Title>
             </a>
+
+            <Card.Text as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </Card.Text>
 
             <Card.Text as="h3">
                 ${product.price}
