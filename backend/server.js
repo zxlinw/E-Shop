d4d46709e75c859e54e6cc2466cb2b9fadd59b92
Zilin <hannahzweng@gmail.com>
2024-08-12 00:18:55 -0400

    express setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import products from './data/products.js';
+const port = 2000;
+
+const app = express();
+
+app.get('/', (req, res) => { 
+    res.send('API is running');
+ });
+
+ app.get('/api/products', (req, res) => {
+    res.json(products);
+ })
+
+ app.listen(port, () => console.log(`server running on port ${port}`));
