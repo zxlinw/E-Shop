e123edd27820f9935866ad15f567dd74f54b0d8f
Zilin <hannahzweng@gmail.com>
2024-08-20 18:41:18 -0400

    fetching single products

--- frontend/src/screens/Productscreen.jsx
+++ frontend/src/screens/Productscreen.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom'
-import products from '../products'
+import axios from 'axios';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
 
 const Productscreen = () => {
-    const {id} = useParams();
-    const product = products.find((p) => p._id === id);
+    const {id: productId} = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+
+    }, [productId]);
 
   return (
     <>
