5879bd030576f1918331f6b880aa3a512fd60bad
Zilin <hannahzweng@gmail.com>
2024-08-20 23:47:13 -0400

    seeding script start

---
+++ backend/seeder.js
@@ -0,0 +1,13 @@
+// script to seed dummy data from js files to mongodb database
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+
+
+
