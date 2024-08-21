9c7e1636411045e0e39647790d031826766e320b
Zilin <hannahzweng@gmail.com>
2024-08-20 22:57:36 -0400

    mongoose connecting to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const port = process.env.PORT || 2000;
+
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => { 
