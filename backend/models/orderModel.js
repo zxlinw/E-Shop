676518fa19571098a1638316f2f36c5f29ec6a2d
Zilin <hannahzweng@gmail.com>
2024-08-22 14:23:54 -0400

    data added

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -13,7 +13,7 @@ const orderSchema = new mongoose.Schema({
             image: {type: String, required: true},
             price: {type: Number, required: true},
             product: {
-                tyoe: mongoose.Schema.Types.ObjectId,
+                type: mongoose.Schema.Types.ObjectId,
                 required: true,
                 ref: "Product",
             }
