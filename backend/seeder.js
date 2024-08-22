1384b2d4cb17774c621b56b760f1c4f62f6b39a8
Zilin <hannahzweng@gmail.com>
2024-08-22 13:59:41 -0400

    adding dummy data to mongodb with seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,5 +1,4 @@
 // script to seed dummy data from js files to mongodb database
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -8,6 +7,39 @@ import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        // inserts users from js file into the db
+        const createdUsers = await User.insertMany(users);
+
+        // extracts the only admin user from the returned array
+        const adminUser = createdUsers[0]._id;
+
+        // creates a new array with same products as in js file but with additional user field
+        const sampleProducts = products.map((product) => {
+            return { ...product, user: adminUser };
+        });
+
+        // inserts the new products array
+        await Product.insertMany(sampleProducts);
+
+        console.log('Data imported'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+
+    }
+}
+
 
 
 
