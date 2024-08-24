6c718460ced5373c40c9b789e120e453ed7552de
Zilin <hannahzweng@gmail.com>
2024-08-23 23:51:42 -0400

    message component

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import {FaTrash} from 'react-icons/fa';
+import Message from 
+
+const CartScreen = () => {
+  return (
+    <div>CartScreen</div>
+  )
+}
+
+export default CartScreen
