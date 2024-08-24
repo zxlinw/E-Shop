a6f4ec24fbb8dfcc0c7f63deea20ba82480e3540
Zilin <hannahzweng@gmail.com>
2024-08-23 23:47:51 -0400

    cart header selecting from global state

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser} from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import {useSelector} from 'react-redux';
 
 const Header = () => {
+  const {cartItems} = useSelector((state) => state.cart);
+  
+
+
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -12,7 +17,14 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                        <Nav.Link href="/cart"><FaShoppingCart/> Cart
+                        {
+                            cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                    {cartItems.reduce((acc, current) => acc + current.qty, 0)}
+                                </Badge>
+                            )
+                        }</Nav.Link>
                         <Nav.Link href="/login"> <FaUser /> User</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
