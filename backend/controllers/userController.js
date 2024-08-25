c7b4349386148531032237831fc1915016e2d3c1
Zilin <hannahzweng@gmail.com>
2024-08-25 13:41:48 -0400

    logout and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // logs user out and clears the cookie
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({message: 'Logged out successfully'});
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
