f84d04b9d24d26dca3843a64cdc8489e1f5cc033
Zilin <hannahzweng@gmail.com>
2024-08-25 14:51:58 -0400

    user profiles

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import { UNSAFE_ViewTransitionContext } from 'react-router-dom';
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
@@ -9,7 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
     if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id);
 
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -37,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (user) {
         generateToken(res, user._id);
-        
+
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -61,11 +62,45 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+    
 });
 
 const getUsers = asyncHandler(async (req, res) => {
