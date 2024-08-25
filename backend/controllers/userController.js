e02a4f33fa547dbdfeb884ac972ac2dbb3a4d115
Zilin <hannahzweng@gmail.com>
2024-08-25 12:55:02 -0400

    validate password with schema models

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body;
     const user = await User.findOne({email: email});
 
-    if(user) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
